2e65bfcff73b0c870b74d6a4012b216d3ee363dd
ale <alessandro.guerriero@live.it>
2024-05-30 15:16:55 +0200

    fixed centerline porcessor

--- src/cprocessor.py
+++ src/cprocessor.py
@@ -8,16 +8,21 @@ import numpy as np
 import cv2 as cv
 import math
 
+import rospy
+
 class CameraProcessor:
     """
-    Class for visualizing the processed camera info.
+    Class for processing and visualizing camera info.
     """
+
     def __init__(
             self,
-            show_img: bool = False
+            debug: bool = False
     ) -> None:
+        self.height = 480
+        self.width = 640
         self.canvas = None
-        self.show_img = show_img
+        self.debug = debug
         self.cv_bridge = CvBridge()
 
     def process(
@@ -36,21 +41,20 @@ class CameraProcessor:
         img = self._get_track_outline(
             self.cv_bridge.imgmsg_to_cv2(img_msg, desired_encoding="bgr8")
         )
-        centerline = self._get_centerline(img)
+        left, right, centerline = self._get_centerline(img)
         
-        if self.show_img:
-            self.canvas = img
-            self._draw_centerline(centerline)
+        if self.debug:
+            self._draw(left, right, centerline)
             self.show()
 
         return centerline
 
-    @staticmethod
     def _get_track_outline(
+            self,
             img: np.array,
     ) -> np.ndarray:
         img = img[int(img.shape[0]/2):img.shape[0]-10, 100:img.shape[1]-100]
-        img = cv.resize(img, (640, 480))
+        img = cv.resize(img, (self.width, self.height))
         track_outline = np.zeros((img.shape[0], img.shape[1]), dtype=np.uint8)
         img_mask = colors.mask(img, np.array(colors.LOWER_YELLOW), np.array(colors.UPPER_YELLOW))
 
@@ -60,38 +64,51 @@ class CameraProcessor:
 
         return track_outline
     
+    @staticmethod
     def _get_centerline(
-            self,
             track_outline: np.ndarray
-    ) -> np.array:
+    ) -> tuple[np.array, np.array, np.array]:
         centerline = []
-        left, right = self._get_track_limits(track_outline)
-        for (x1, y1), (x2, y2) in list(zip(left, right)):
+        left, right = CameraProcessor._get_track_limits(track_outline)
+        for (x1, y1), (x2, y2) in list(zip(left[::10], right[::10])):
             xc = math.floor((x1+x2)/2)
             yc = math.floor((y1+y2)/2)
+            rospy.loginfo(f'xc: {xc}, yc: {yc}')
             centerline.append((xc, yc))
-        return np.array(centerline)
+        return left, right, np.array(centerline)
 
     @staticmethod
     def _get_track_limits(
             track_outline: np.ndarray
     ):
-        _, labels = cv.connectedComponents(track_outline)
+        _, labels = cv.connectedComponents(track_outline[1:-1, 1:-1])
         left_limit = np.column_stack(
             np.where(labels==1)
         )
+        rospy.loginfo(f'unique: {np.unique(labels)}')
         right_limit = np.column_stack(
             np.where(labels==2)
         )
+        rospy.loginfo(f'right: {right_limit}')
 
         return left_limit, right_limit
     
-    def _draw_centerline(
+    def _draw(
             self,
+            left: np.array,
+            right: np.array,
             centerline: np.array
     ) -> None:
-        for point in centerline:
-            cv.circle(self.canvas, point, colors.MAGENTA, 1)
+        self.canvas = np.zeros((self.height, self.width, 3), dtype=np.uint8)
+        
+        for (x, y) in left:
+            cv.circle(self.canvas, (y, x), 1, colors.WHITE)
+
+        for (x, y) in right:
+            cv.circle(self.canvas, (y, x), 1, colors.WHITE)
+
+        for (x, y) in centerline:
+            cv.circle(self.canvas, (y, x), 1, colors.MAGENTA)
 
     def show(self):
         cv.imshow("Visualize", self.canvas)
